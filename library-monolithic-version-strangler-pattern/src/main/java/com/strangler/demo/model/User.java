cfffa02c2f5f17d8f44d5587d337834a5ff99896
AyyoubTelmoudy <ayyoub.telmoudy@gmail.com>
2023-05-31 12:54:52 +0200

    remove jpa annotation

--- library-monolithic-version-strangler-pattern/src/main/java/com/strangler/demo/model/User.java
+++ library-monolithic-version-strangler-pattern/src/main/java/com/strangler/demo/model/User.java
@@ -7,15 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
